3d7d76854ff8b39ac67127987d73aac2ab7f38b4
YuanlongHu <huyuanlong1996@163.com>
2022-05-27 12:51:07 +0800

    update read_bib

--- R/read_bib.R
+++ R/read_bib.R
@@ -7,6 +7,7 @@
 #' @importFrom stringr str_detect
 #' @importFrom dplyr %>%
 #' @importFrom dplyr recode
+#' @importFrom dplyr distinct
 #' @return A list.
 #' @author Yuanlong Hu
 #' @export
@@ -17,12 +18,12 @@ read_bib <- function(file){
   data <- read_lines(file = file, skip = 0, n_max = -1L)
 
   n <- list(
-  RT = "^RT ",
-  T1 = "^T1 ", A1 = "^A1 ",
-  YR = "^YR ", JF = "^JF ",
-  AB = "^AB ", K1 = "^K1 ",
-  AD = "^AD ", SN = "^SN ",
-  DS = "^DS "
+   RT = "^RT ",
+   T1 = "^T1 ", A1 = "^A1 ",
+   YR = "^YR ", JF = "^JF ",
+   AB = "^AB ", K1 = "^K1 ",
+   AD = "^AD ", SN = "^SN ",
+   DS = "^DS "
 )
 
 ck <- lapply(n, function(x){
@@ -49,6 +50,19 @@ for (i in 1:length(end)) s[[i]] <- c(start[i], end[i])
  data2 <- as.data.frame(Reduce(rbind,data2))
  #data2$source <- source
  data2$DS <- recode(data2$DS, "万方数据"="WanFang", "重庆维普"="VIP")
+ t <- as.data.frame(table(data2$DS))
+ message(paste0("----- 1 ----- ",
+                 "\nCNKI: ", t[t$Var1=="CNKI",2],
+                "\nWanFang: ", t[t$Var1=="WanFang",2],
+                "\nVIP: ", t[t$Var1=="VIP",2]
+                ))
+ data2 <- distinct(data2, T1, JF, YR, .keep_all = TRUE)
+ t <- as.data.frame(table(data2$DS))
+ message(paste0("----- 2 ----- ",
+                "\nCNKI: ", t[t$Var1=="CNKI",2],
+                "\nWanFang: ", t[t$Var1=="WanFang",2],
+                "\nVIP: ", t[t$Var1=="VIP",2]
+ ))
  data2$ID <- paste0(data2$JF, data2$YR,"_", 1:nrow(data2))
  return(data2)
 }
